aca7fb81145c41bf060ffa4a9d5ec2a7abff4085
illia-k <illia.kyeylenko@gmail.com>
2015-09-10 23:22:06 +0300

    add model

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,17 @@
 /// <reference path="lib/backbone-min.js" />
 /// <reference path="lib/underscore-min.js" />
 $(function(){
-	
+		
 	var Service = Backbone.Model.extend({
 		
-	});	
+		defaults:{
+			title: 'My service',
+			price: 100,
+			checked: false
+		},
+		
+		toggle: function(){
+			this.set('checked', !this.get('checked'));
+		}	
+	});		
 });

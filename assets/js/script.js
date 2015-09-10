2fe88ce7d99e9088156e301eaaf2d5526ab98fe7
illia-k <illia.kyeylenko@gmail.com>
2015-09-10 08:59:06 +0300

    init commit

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+/// <reference path="lib/jquery.min.js" />
+/// <reference path="lib/backbone-min.js" />
+/// <reference path="lib/underscore-min.js" />
+$(function(){
+	
+	var Service = Backbone.Model.extend({
+		
+	});	
+});
